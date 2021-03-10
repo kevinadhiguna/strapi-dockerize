0415a217d4e04a7d5410b2ae209e313fb85df9a0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-03-10 09:39:08 +0700

    feat: add Amazon SES as an email provider plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,16 @@
 module.exports = ({ env }) => ({
+    email: {
+        provider: 'amazon-ses',
+        providerOptions: {
+            key: env('AWS_ACCESS_KEY_ID'),
+            secret: env('AWS_SECRET_ACCESS_KEY'),
+            amazon: `https://email.${env('AWS_REGION')}.amazonaws.com`
+        },
+        settings: {
+            defaultFrom: env('EMAIL_DEFAULT_FROM'),
+            defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO')
+        }
+    },
     upload: {
         provider: 'cloudinary',
         providerOptions: {
