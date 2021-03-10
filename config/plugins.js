4785dc3c83b2602d3a86bc2ae5f5734d5177a082
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-03-10 23:25:35 +0700

    refactor: change an environment variable name

--- config/plugins.js
+++ config/plugins.js
@@ -17,7 +17,7 @@ module.exports = ({ env }) => ({
     provider: 'aws-s3',
     providerOptions: {
       accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
       region: env('AWS_REGION'),
       params: {
         Bucket: env('AWS_BUCKET_NAME'),
