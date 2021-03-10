0a20fe2e9d7443e5806d4f3646fd8b8f3cfbd52d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-03-10 09:47:36 +0700

    style: format code using Prettier

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,22 @@
 module.exports = ({ env }) => ({
-    email: {
-        provider: 'amazon-ses',
-        providerOptions: {
-            key: env('AWS_ACCESS_KEY_ID'),
-            secret: env('AWS_SECRET_ACCESS_KEY'),
-            amazon: `https://email.${env('AWS_REGION')}.amazonaws.com`
-        },
-        settings: {
-            defaultFrom: env('EMAIL_DEFAULT_FROM'),
-            defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO')
-        }
+  email: {
+    provider: 'amazon-ses',
+    providerOptions: {
+      key: env('AWS_ACCESS_KEY_ID'),
+      secret: env('AWS_SECRET_ACCESS_KEY'),
+      amazon: `https://email.${env('AWS_REGION')}.amazonaws.com`
     },
-    upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-            cloud_name: env('CLOUD_NAME', ''),
-            api_key: env('API_KEY', ''),
-            api_secret: env('API_SECRET', '')
-        }
-    },
+    settings: {
+      defaultFrom: env('EMAIL_DEFAULT_FROM'),
+      defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO')
+    }
+  },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUD_NAME', ''),
+      api_key: env('API_KEY', ''),
+      api_secret: env('API_SECRET', '')
+    }
+  },
 });
