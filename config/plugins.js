357c4bc5b625f3bbb9235e58d18a2b518b3186b4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-30 14:59:23 +0700

    refactor(upload): change upload provider to Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -13,26 +13,26 @@ module.exports = ({ env }) => ({
   },
   // == Please choose either for upload ==
   // AWS Simple Storage Service (S3) configuration
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
-      region: env('AWS_REGION'),
-      params: {
-        Bucket: env('AWS_BUCKET_NAME'),
-        StorageClass: env('AWS_S3_STORAGE_CLASSES') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
-      },
-      logger: console // Only if you want to `stdout` logs
-    }
-  },
-  // Cloudinary configuration
   // upload: {
-  //   provider: 'cloudinary',
+  //   provider: 'aws-s3',
   //   providerOptions: {
-  //     cloud_name: env('CLOUD_NAME', ''),
-  //     api_key: env('API_KEY', ''),
-  //     api_secret: env('API_SECRET', '')
+  //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
+  //     secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
+  //     region: env('AWS_REGION'),
+  //     params: {
+  //       Bucket: env('AWS_BUCKET_NAME'),
+  //       StorageClass: env('AWS_S3_STORAGE_CLASSES') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
+  //     },
+  //     logger: console // Only if you want to `stdout` logs
   //   }
   // },
+  // Cloudinary configuration
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUD_NAME', ''),
+      api_key: env('API_KEY', ''),
+      api_secret: env('API_SECRET', '')
+    }
+  },
 });
