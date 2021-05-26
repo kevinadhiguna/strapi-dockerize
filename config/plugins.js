12c504cff4769a624232d41b42a32a394097cc00
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-26 10:52:12 +0700

    refactor(S3): change AWS S3 Bucket env variable

--- config/plugins.js
+++ config/plugins.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => ({
   //     secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
   //     region: env('AWS_REGION'),
   //     params: {
-  //       Bucket: env('AWS_BUCKET_NAME'),
+  //       Bucket: env('AWS_BUCKET'),
   //       StorageClass: env('AWS_S3_STORAGE_CLASSES') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
   //     },
   //     logger: console // Only if you want to `stdout` logs
